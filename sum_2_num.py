1fc004c922dd2041ae9998e1637198afcaa64a48
Shobhit <132779318+Shobhit-20-04@users.noreply.github.com>
2024-10-14 22:55:52 +0530

    Update sum_2_num.py

--- sum_2_num.py
+++ sum_2_num.py
@@ -1,4 +1,4 @@
 num1 = int(input("Enter Your First number="))
-num2 = int(input("Enter Your First number="))
+num2 = int(input("Enter Your Second number="))
 num3=num1+num2;
 print("Sum of",num1,"and",num2,"is",num3)
